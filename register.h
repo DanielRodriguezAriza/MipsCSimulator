c31e865e5f3a9d008ba6a2219c695ec82636d4e6
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-22 17:35:00 +0100

    Added double precission floating point register bank struct

--- register.h
+++ register.h
@@ -71,4 +71,8 @@ typedef struct {
 	float registers[32]; // for double precission in MIPS32, we use register pairing, so $f0:$f1 is the pair used for the first double precission floating point "register", which consumes 2 single precission floating point registers. Not sure how real MIPS handles pairing if I pick an odd register number rather than pairing from even numbers, I'll figure this reading some documentation later I suppose.
 } register_bank_float;
 
+typedef struct {
+	double registers[16]; // we cast to this type later on through either type punning or unions, we'll see.
+} register_bank_double;
+
 #endif
