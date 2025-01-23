b2df929cfe8670b4a70b3e7ab26df34a5bbc238c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-23 04:10:05 +0100

    Modified example program in main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main()
 {
 	vm_t vm;
-	vm.debug_enabled = true;
+	vm.debug_enabled = false;
 	instruction_t instructions[] = {
 		{MOVE, $t0, $zero},
 		{MOVE, $t1, $zero},
@@ -14,8 +14,18 @@ int main()
 		{MOVE, $a0, $t2},
 		{SYSCALL},
 		{ADDI, $v0, $zero, SYSCALL_PRINT_CHARACTER},
-		{ADDI, $a0, $zero, '!'},
+		{ADDI, $a0, $zero, '\n'},
 		{SYSCALL},
+		{LI, $t0, 10},
+		{LI, $t1, 0},
+		{LI, $v0, SYSCALL_PRINT_INTEGER},
+		{MOVE, $a0, $t1},
+		{SYSCALL},
+		{LI, $v0, SYSCALL_PRINT_CHARACTER},
+		{LI, $a0, '\n'},
+		{SYSCALL},
+		{ADDI, $t1, $t1, 1},
+		{BNE, $t0, $t1, 12},
 		{ADDI, $a0, $zero, 101},
 		{ADDI, $v0, $zero, SYSCALL_EXIT2},
 		{SYSCALL},
